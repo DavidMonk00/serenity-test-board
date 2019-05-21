a4b6b090af9503682fea070965c47036e7333897
David Monk <david.gabriel.monk@cern.ch>
2019-05-21 11:42:51 +0100

    Added try catch block

--- FTDI/src/main.cpp
+++ FTDI/src/main.cpp
@@ -182,14 +182,16 @@ int main(int argc, char** argv) {
 
     if (sweep_flag) {
         for (uint8_t addr = 0; addr < 0x100; addr++) {
-            std::vector<uint8_t> rData;
-            rData.resize(ndata);
-            stb->ftdi->send(addr|I2C_RD, {}, rData);
-            std::cout << addr << '\n';
-            printf("Reading operation done, data read:\n");
-            for(auto i : rData) {
-                printf("0x%x\n", i);
-            }
+            try {
+                std::vector<uint8_t> rData;
+                rData.resize(ndata);
+                stb->ftdi->send(addr|I2C_RD, {}, rData);
+                std::cout << addr << '\n';
+                printf("Reading operation done, data read:\n");
+                for(auto i : rData) {
+                    printf("0x%x\n", i);
+                }
+            } catch {}
         }
     }
 
